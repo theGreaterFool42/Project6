e4a73db2289b0ae469f431b9e5312c1441d5dd8d
theGreaterFool42 <h.lippka@googlemail.com>
2016-04-25 02:31:14 +0200

    Anfang
    StartScene
    - Anzahl der Player festlegen
    
    MainScene
    - Player werden geladen
    - Player können auf der Welt rumlaufen

---
+++ SphereGravityDemo/Assets/Scripts/GravityBody.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityBody : MonoBehaviour
+{
+    public Attractor attractor;
+    private Transform transformObject;
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        this.GetComponent<Rigidbody>().useGravity = false;
+        transformObject = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attractor.Attract(transformObject);
+    }
+}
---
+++ SphereGravityDemo/Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController2 : MonoBehaviour
+{
+
+    public float moveSpeed = 15;
+    public float jumpSpeed = 250;
+    private Vector3 moveDirection;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal2"), 0, Input.GetAxisRaw("Vertical2")).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump2"))
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpSpeed, 0));
+            print("Jump2");
+        }
+    }
+}
