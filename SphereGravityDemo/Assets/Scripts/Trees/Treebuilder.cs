b8a59b3c73bb1fd9949ffef3fbab2f9526bc8acb
Altezicon <Jannik.mentzel@gmail.com>
2016-05-03 20:27:13 +0200

    Bäume implementiert mit Joints
    Die Bäume sind mit Joints zusammengebaut.
    
    Hab ein Script angefangen, dass die Bäume selbst zusammenstellen kann.

---
+++ SphereGravityDemo/Assets/Scripts/Trees/Treebuilder.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class Treebuilder : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        GameObject tree = new GameObject("tree");
+        tree.AddComponent<Rigidbody>();
+        tree.AddComponent<MeshRenderer>();
+        tree.AddComponent<BoxCollider>();
+        tree.AddComponent<MeshFilter>();
+        
+        Mesh mesh = (Mesh)Resources.Load("Assets/Models/Baumstamm", typeof(Mesh));
+        //public MeshFilter mf;
+        tree.GetComponent<MeshFilter>().mesh = mesh;
+
+        //ImportExample();
+        //AssetDatabase.Refresh();
+
+    }
+
+    [MenuItem("AssetDatabase/ImportExample")]
+    static void ImportExample()
+    {
+        //AssetDatabase.ImportAsset("Assets/Models/Baumstamm.fbx", ImportAssetOptions.Default);
+        
+
+
+    }
+    // Update is called once per frame
+    void Update () {
+
+
+
+
+        
+    }
+   
+}
